3c6f5f324383272a3eb06945e646b2b67b177aa1
lucas <lucas.pfranca01@gmail.com>
2026-08-21 11:43:06 -0300

    feat: update random names

--- src/utils/nameGenerator.ts
+++ src/utils/nameGenerator.ts
@@ -1,18 +1,27 @@
-const nomes = [
-    'Capivara', 'Camper', 'ProPlayer', 'Noob',
-    'Prata', 'Global', 'Looter', 'Rushador',
-    'Boludo', 'Sniper', 'Pombo', 'Troll'
+const names = [
+    'Camper', 'ProPlayer', 'Noob',
+    'Prata', 'Global', 'Rushador',
+    'Boludo', 'Sniper', 'Troll',
+    'Zé Casinha', 'Zé Lootinho', 'Bot', 'Smurf',
+    'Cone', 'Mochila', 'Bagre', 'Rato',
+    'Xitado', 'Flasheado', 'Pescador', 'Paraquedista',
+    'AFK', 'Tiltado',
 ];
 
-const sufixos = [
+const suffix = [
     'Cego', 'Lagado', 'da AWP', 'da Panela',
     'de Miramar', 'Pino', 'Míope', 'da AK',
-    'Tóxico', 'Tryhard', 'de Pochinki', 'Frio'
+    'Tóxico', 'Tryhard', 'de Pochinki', 'Frio',
+    'do Rush B', 'da Red Zone', 'do Gás', 'da Dust 2',
+    'da C4', 'Cego de Flash', 'Atropelado pelo Buggy',
+    'sem Colete', 'Pescador de Pixel',
+    'sem Robux', 'do Blox Fruits', 'sem Fone', 'Tiltado',
+    'Carregado', 'Mutado', 'Ping 999', 'do Teclado RGB'
 ];
 
 export function generateRandomName(): string {
-    const nomeRandom = nomes[Math.floor(Math.random() * nomes.length)];
-    const sufixoRandom = sufixos[Math.floor(Math.random() * sufixos.length)];
+    const nomeRandom = names[Math.floor(Math.random() * names.length)];
+    const sufixoRandom = suffix[Math.floor(Math.random() * suffix.length)];
 
     const numero = Math.floor(Math.random() * 99) + 1;
 
